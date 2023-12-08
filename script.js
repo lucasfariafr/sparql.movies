1b562eb6c7de3e2b6f178df85d3e4b02a52810f6
lucasfariafr <lucas.faria.fr@gmail.com>
2023-12-08 19:12:23 +0100

    add stick graph

--- script.js
+++ script.js
@@ -1,36 +1,32 @@
-// création du graphe à barres
-$(document).ready(function(){
-
-    var data = {
-        labels: ["Catégorie 1", "Catégorie 2", "Catégorie 3"], // noms des catégories
-        datasets: [{
-            label: 'Valeurs',
-            data: [10, 20, 15], // valeurs correspondant aux catégories
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)'
-            ],
-            borderWidth: 1
-        }]
-    };
-
-    // créer le graphe à barres
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var myChart = new Chart(ctx, {
-        type: 'bar',
-        data: data,
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
-    });
-});
+// Données factices pour le graphique
+const data = {
+    labels: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai'],
+    datasets: [
+      {
+        label: 'Ventes mensuelles',
+        backgroundColor: 'rgba(54, 162, 235, 0.5)',
+        borderColor: 'rgba(54, 162, 235, 1)',
+        borderWidth: 1,
+        data: [20, 35, 40, 25, 30],
+      },
+    ],
+  };
+  
+  // Configuration du graphique
+  const config = {
+    type: 'bar',
+    data: data,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  };
+  
+  // Récupération du contexte du canevas
+  const ctx = document.getElementById('barChart').getContext('2d');
+  
+  // Création du graphique à barres
+  const myChart = new Chart(ctx, config);
