2b7886e7e4dfb9bd2b47223a6bcc363de2d80a42
lucasfariafr <lucas.faria.fr@gmail.com>
2023-12-08 20:04:55 +0100

    add stick graph

--- script.js
+++ script.js
@@ -1,40 +1,32 @@
 // Données factices pour le graphique
 const data = {
-    labels: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai'],
-    datasets: [
-      {
-        label: 'Ventes mensuelles',
-        backgroundColor: 'rgba(54, 162, 235, 0.5)',
-        borderColor: 'rgba(54, 162, 235, 1)',
-        borderWidth: 1,
-        data: [20, 35, 40, 25, 30],
-      },
-    ],
-  };
-  
-  // Configuration du graphique
-  const config = {
-    type: 'bar',
-    data: data,
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true,
-        },
+  labels: ["Janvier", "Février", "Mars", "Avril", "Mai"],
+  datasets: [
+    {
+      label: "Ventes mensuelles",
+      backgroundColor: "rgba(54, 162, 235, 0.5)",
+      borderColor: "rgba(54, 162, 235, 1)",
+      borderWidth: 1,
+      data: [20, 35, 40, 25, 30],
+    },
+  ],
+};
+
+// Configuration du graphique
+const config = {
+  type: "bar",
+  data: data,
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
       },
     },
-  };
-  
-  // Récupération du contexte du canevas
-  const ctx = document.getElementById('barChart').getContext('2d');
-  
-  // Création du graphique à barres
-  const myChart = new Chart(ctx, config);
+  },
+};
 
-  $(document).ready(function () {
-    // Charge la barre de navigation
-    $("#navbar-container").load("view/navbar.html");
-  
-    // Charge le pied de page
-    $("#footer-container").load("view/footer.html");
-  });
+// Récupération du contexte du canevas
+const ctx = document.getElementById("barChart").getContext("2d");
+
+// Création du graphique à barres
+const myChart = new Chart(ctx, config);
